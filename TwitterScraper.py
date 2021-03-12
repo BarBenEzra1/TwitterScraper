ece9f4bcfa83bb5a9e12e55b04f67447fca9d241
BarBenEzra1 <75648208+BarBenEzra1@users.noreply.github.com>
2021-03-12 06:17:55 +0200

    Update TwitterScraper.py

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 import time
-from random import randint
 import os
 import sys
 
